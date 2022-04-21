f93a2cdd7b40d41241558a6a2cd4df50b1397e3a
rrodrigues77 <ricardo.engenharia.com@gmail.com>
2022-04-20 22:28:44 -0300

    Incluso a linha do package find linha19

--- setup.py
+++ setup.py
@@ -9,13 +9,14 @@ with open("requirements.txt") as f:
 setup(
     name="image_processing-test",
     version="0.0.1",
-    author="Ricardo_Rodrigues",
+    author="Ricardo Rodrigues",
     author_email="ricardo.engenharia.com@gmail.com",
     description="Descricao do pacote de Imagens com Python. Projeto original de Karina Tiemi Kato, Tech Lead, Machine Learning Engineer, Data Scientist Specialist at Take. This package is a demo for simulation of upload on the Test Pypi website, and it's from class of the Bootcamp developer full stack Python. E-mail:karinatkato@gmail.com.",
     long_description=page_description,
     long_description="text/markdown",
     long_description_content_type="text/markdown",
-    url="https://github.com/rrodrigues77/Criar-pacotes-de-processamento-de-imagens-em-Python"
+    url="https://github.com/rrodrigues77/Criar-pacotes-de-processamento-de-imagens-em-Python",
+    packages=find_packages(),
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
